46d09ee6ac7cdd021d9057fd76c4afe777ca8bee
Rishu <rishabhkaparwan8@gmail.com>
2025-01-17 18:58:57 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import pickle 
+import tensorflow as tf
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import streamlit as st
